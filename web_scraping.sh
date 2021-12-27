cd7850a51c8978b7a0a1fc345a9d13d2c39ac7f4
Nelleke Smits <nelleke_s@hotmail.com>
2021-12-27 14:27:05 +0100

    fix(scripts): add correct path

--- web_scraping.sh
+++ web_scraping.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd /home/pi/Documents/house-scraper-cron-job/
-source /home/pi/Documents/house-scraper-cron-job/venv/bin/activate 
-/home/pi/Documents/house-scraper-cron-job/venv/bin/python3.9 /home/pi/Documents/house-scraper-cron-job/src/web_scraping.py R,1
+source venv/bin/activate 
+venv/bin/python3.9 src/web_scraping.py R,1
