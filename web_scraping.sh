a9a816c9718ff6c8bbf8ba9e4812c5ca35d0f2bb
Nelleke Smits <nelleke_s@hotmail.com>
2021-12-27 14:20:48 +0100

    add scripts for conjob

---
+++ web_scraping.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+cd /home/pi/Documents/house-scraper-cron-job/
+source /home/pi/Documents/house-scraper-cron-job/venv/bin/activate 
+/home/pi/Documents/house-scraper-cron-job/venv/bin/python3.9 /home/pi/Documents/house-scraper-cron-job/src/web_scraping.py R,1
