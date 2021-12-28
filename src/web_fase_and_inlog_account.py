58d25ae4299fd235e17d196631725c0314d4922e
Nelleke Smits <nelleke_s@hotmail.com>
2021-12-28 10:32:54 +0100

    fix(linting): add flake8 lintingstyle

--- src/web_fase_and_inlog_account.py
+++ src/web_fase_and_inlog_account.py
@@ -1,10 +1,8 @@
-import bs4, requests, smtplib,re, json
+from bs4 import BeautifulSoup
+import requests
+import re
 from selenium import webdriver
-import numpy as np
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC 
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 from pathlib import Path
@@ -13,15 +11,15 @@ import configparser
 
 
 def check_phase_status(config):
-    #check is phase 2 is available by selecting all phases available in the preparation phase
+    # check is phase 2 is available by selecting all phases
     chrome_options = Options()
-    chrome_options.add_argument("--headless")          
+    chrome_options.add_argument("--headless")
     url = config['default']['url_1']
-    browser = webdriver.Chrome(options=chrome_options)    
+    browser = webdriver.Chrome(options=chrome_options)
     browser.get(url)
     html = browser.page_source
-    woning_soup = bs4.BeautifulSoup(html, 'lxml')
-    fase_2 = woning_soup.find_all('span',attrs={'class':'property-card__project'})
+    woning_soup = BeautifulSoup(html, 'lxml')
+    fase_2 = woning_soup.find_all('span', attrs={'class': 'property-card__project'})
     output = []
     for i in fase_2:
         output.append(re.findall(r"\>(.*)\<", str(i), flags=0))
@@ -30,33 +28,34 @@ def check_phase_status(config):
     browser.quit()
     return output_flat
 
-def sign_in_new_phase(config, phase_output, housenumbers, chatbot_id, chatbot_key, chatbot_id2, chatbot_key2):
+
+def sign_in_new_phase(config, phase_output, housenumbers, chatbot_id, chatbot_key):
     logging.info('The following users are in the userset: {}'.format(usernames))
     logging.info('Running the applicationform sign-up script..')
 
-    #send output to telegram bot
+    # send output to telegram bot
     resulttext = []
     resulttext.append(set(phase_output))
     resulttext.append('is also available in the totalset, check it out online. If not done yet, you will now be signed in.')
     requests.get("https://api.telegram.org/{}:{}/sendMessage?chat_id={}&text={}".format(chatbot_id, chatbot_key, config[user]['telegram_id'], resulttext))
 
-    #running script per new housenumber      
+    # running script per new housenumber
     for housenumber in housenumbers:
 
-        #check if already signed in
-        screenshot_name = "./screenshot/{}_screenshot_signup{}.png".format(user,housenumber)
+        # check if already signed in
+        screenshot_name = "./screenshot/{}_screenshot_signup{}.png".format(user, housenumber)
         screenshot_confirmation = Path(screenshot_name)
         if screenshot_confirmation.is_file():
-            logging.info('For the combination of {} and {}, a file already exists, so will skip this run.'.format(user, housenumber))                   
+            logging.info('For the combination of {} and {}, a file already exists, so will skip this run.'.format(user, housenumber))
             continue
-                        
-        #if not, sign in to webpage
+
+        # if not, sign in to webpage
         else:
-            #send starting info to telegram
+            # send starting info to telegram
             output_combinatie = 'The following combination is filled out {}, {}'.format(user, housenumber)
             requests.get("https://api.telegram.org/{}:{}/sendMessage?chat_id={}&text={}".format(chatbot_id, chatbot_key, config[user]['telegram_id'], output_combinatie))
 
-            #options for the chromedriver
+            # options for the chromedriver
             chrome_options = Options()
             chrome_options.add_argument("--window-size=1920,1080")
             chrome_options.add_argument("--disable-extensions")
@@ -68,17 +67,17 @@ def sign_in_new_phase(config, phase_output, housenumbers, chatbot_id, chatbot_ke
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--ignore-certificate-errors')
-            url = "https://www.nieuwbouwinhouten.nl/woningen//tussenwoning-herenhuis-woningtype-a077R00001JFZFIQA5/bouwnummer-{}".format(housenumber)                 
-            browser = webdriver.Chrome(chrome_options=chrome_options)            
+            url = "https://www.nieuwbouwinhouten.nl/woningen//tussenwoning-herenhuis-woningtype-a077R00001JFZFIQA5/bouwnummer-{}".format(housenumber)
+            browser = webdriver.Chrome(chrome_options=chrome_options)
             browser.get(url)
             browser.implicitly_wait(10)
 
-            #accept cookies
+            # accept cookies
             python_button1 = browser.find_element_by_xpath('/html/body/bpd-cookies/div/div/div/div/button')
             logging.info('Button 1 is clicked - accepting cookies on the website for the combination {}, {}.'.format(user, housenumber))
             python_button1.click()
-    
-            #click on projectfase button
+
+            # click on projectfase button
             action = ActionChains(browser)
             browser.execute_script("window.scrollTo(0, 750)") 
             time.sleep(3)
@@ -86,7 +85,7 @@ def sign_in_new_phase(config, phase_output, housenumbers, chatbot_id, chatbot_ke
             logging.info('Button 2 is clicked - click on the sign-up button for the combination {}, {}.'.format(user, housenumber))
             action.move_to_element(python_button2).click().perform()
 
-            #select and send keys from the  application form - first step
+            # select and send keys from the  application form - first step
             browser.find_element_by_id("lead-form-firstname").send_keys(config[user]["username"])
             browser.find_element_by_id("lead-form-lastname").send_keys(config[user]["lastname"])
             browser.find_element_by_id("lead-form-email").send_keys(config[user]["emailadres"])
@@ -95,11 +94,11 @@ def sign_in_new_phase(config, phase_output, housenumbers, chatbot_id, chatbot_ke
             browser.find_element_by_id("lead-form-housenumber").send_keys(config[user]["housnr"])
             time.sleep(5)
 
-            #select keys from the  application form - second step
+            # select keys from the  application form - second step
             streetname = browser.find_element_by_id("lead-form-streetname")
             place = browser.find_element_by_id("lead-form-place")
 
-            #send keys form the application form - second 
+            # send keys form the application form - second
             time.sleep(3)
             logging.info('Sending the keys in the sign-up form for the combination {}, {}.'.format(user, housenumber))
             streetname.send_keys(config[user]["streetname"])
@@ -109,7 +108,7 @@ def sign_in_new_phase(config, phase_output, housenumbers, chatbot_id, chatbot_ke
             time.sleep(5)
             requests.get("https://api.telegram.org/{}:{}/sendMessage?chat_id={}&text={}".format(chatbot_id, chatbot_key, config[user]['telegram_id'], 'Answers part one and two are sent.'))                    
 
-            #send  application form
+            # send application form
             python_button3 = browser.find_element_by_xpath("/html/body/lead-form/bpd-modal/div/div/bpd-pages/div[1]/bpd-form/form/div[1]/div[14]/button")
             logging.info('Button 3 is clicked - sending the applicationform for the combination {}, {}.'.format(user, housenumber))
             action.move_to_element(python_button3).click().perform()
@@ -118,33 +117,32 @@ def sign_in_new_phase(config, phase_output, housenumbers, chatbot_id, chatbot_ke
             screenshot_confirmation = "./screenshot/{}_screenshot_confirmation{}.png".format(user,housenumber)
             browser.save_screenshot(screenshot_confirmation)
             browser.quit()
-  
+
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
-    config.read('settings.yaml')                #include input from settings
-    config.read('.secrets.yaml')                #include input from secrets
-    logging.basicConfig(level=logging.INFO)     #setup logging
+    config.read('settings.yaml')                # include input from settings
+    config.read('.secrets.yaml')                # include input from secrets
+    logging.basicConfig(level=logging.INFO)     # setup logging
 
     # check if new phases are available
     phase_output = check_phase_status(config)
 
-    # Define settings for running the sign-up script    
+    # Define settings for running the sign-up script
     usernames = [config['default']['user_1'], config['default']['user_2'], config['default']['user_3'], config['default']['user_4']]       
-    chatbot_id = config['default']['chatbot_id']   
-    chatbot_key = config['default']['chatbot_key']   
-    chatbot_id2 = config['default']['chatbot_id2'] 
-    chatbot_key2 = config['default']['chatbot_key2']  
-    phase_check = config['default']['fase_check']              
+    chatbot_id = config['default']['chatbot_id']
+    chatbot_key = config['default']['chatbot_key']
+    chatbot_id2 = config['default']['chatbot_id2']
+    chatbot_key2 = config['default']['chatbot_key2']
+    phase_check = config['default']['fase_check']
 
     # if new phase is availble sign-up for each user for different housenumbers
-    if  phase_check in set(phase_output):
+    if phase_check in set(phase_output):
         for user in usernames:
             housenumbers = [config[user]["housenumber1"], config[user]["housenumber2"]]
             sign_in_new_phase(config, phase_output, housenumbers, chatbot_id, chatbot_key, chatbot_id2, chatbot_key2)
     else:
-        #send message to telegram bot
+        # send message to telegram bot
         resulttext = []
         resulttext.append([set(phase_output), 'is only available in the set.'])
-        requests.get("https://api.telegram.org/{}:{}/sendMessage?chat_id={}&text={}".format(chatbot_id2, chatbot_key2, config['nelleke']['telegram_id'], resulttext))                    
-  
+        requests.get("https://api.telegram.org/{}:{}/sendMessage?chat_id={}&text={}".format(chatbot_id2, chatbot_key2, config['nelleke']['telegram_id'], resulttext))
