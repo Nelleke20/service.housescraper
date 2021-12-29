75e4ff370964227b402795e63983c020588d2abf
Nelleke Smits <nelleke_s@hotmail.com>
2021-12-29 11:12:41 +0100

    fix(error): fix error testing inlog

--- src/web_fase_and_inlog_account.py
+++ src/web_fase_and_inlog_account.py
@@ -3,8 +3,9 @@ import requests
 import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+import time
 from pathlib import Path
 import logging
 import configparser
@@ -79,13 +80,13 @@ def sign_in_new_phase(config, phase_output,
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--ignore-certificate-errors')
             url = "https://www.nieuwbouwinhouten.nl/woningen/tussenwoning-herenhuis-woningtype-a077R00001JFZFIQA5/bouwnummer-{}".format(housenumber)    # noqa: E501
-            browser = webdriver.Chrome(chrome_options=chrome_options)
+            browser = webdriver.Chrome(options=chrome_options)
             browser.get(url)
             browser.implicitly_wait(10)
 
             # accept cookies
-            python_button1 = browser.find_element_by_xpath(
-                '/html/body/bpd-cookies/div/div/div/div/button')
+            python_button1 = browser.find_element(By.XPATH,
+                                                  '/html/body/bpd-cookies/div/div/div/div/button')                                  # noqa: E501
             logging.info('Button 1 is clicked - accepting cookies for the combination {}, {}.'.format(user,                         # noqa: E501
                                                                                                       housenumber))                 # noqa: E501
             python_button1.click()
@@ -94,30 +95,30 @@ def sign_in_new_phase(config, phase_output,
             action = ActionChains(browser)
             browser.execute_script("window.scrollTo(0, 750)")
             time.sleep(3)
-            python_button2 = browser.find_element_by_xpath(
-             "/html/body/main/section[1]/div/div/div[1]/div[2]/div[1]/button")
+            python_button2 = browser.find_element(By.XPATH,
+                                                  "/html/body/main/section[1]/div/div/div[1]/div[2]/div[1]/button")                 # noqa: E501
             logging.info('Button 2 is clicked - click on the sign-up button for the combination {}, {}.'.format(user,               # noqa: E501
                                                                                                                 housenumber))       # noqa: E501
             action.move_to_element(python_button2).click().perform()
 
             # select and send keys from the  application form - first step
-            browser.find_element_by_id("lead-form-firstname").send_keys(
+            browser.find_element(By.ID, "lead-form-firstname").send_keys(
                 config[user]["username"])
-            browser.find_element_by_id("lead-form-lastname").send_keys(
-                [user]["lastname"])
-            browser.find_element_by_id("lead-form-email").send_keys(
+            browser.find_element(By.ID, "lead-form-lastname").send_keys(
+                config[user]["lastname"])
+            browser.find_element(By.ID, "lead-form-email").send_keys(
                 config[user]["emailadres"])
-            browser.find_element_by_id("lead-form-phone").send_keys(
+            browser.find_element(By.ID, "lead-form-phone").send_keys(
                 config[user]["phone"])
-            browser.find_element_by_id("lead-form-postalcode").send_keys(
+            browser.find_element(By.ID, "lead-form-postalcode").send_keys(
                 config[user]["postalcode"])
-            browser.find_element_by_id("lead-form-housenumber").send_keys(
+            browser.find_element(By.ID, "lead-form-housenumber").send_keys(
                 config[user]["housnr"])
             time.sleep(5)
 
             # select keys from the  application form - second step
-            streetname = browser.find_element_by_id("lead-form-streetname")
-            place = browser.find_element_by_id("lead-form-place")
+            streetname = browser.find_element(By.ID, "lead-form-streetname")
+            place = browser.find_element(By.ID, "lead-form-place")
 
             # send keys form the application form - second
             time.sleep(3)
@@ -134,8 +135,8 @@ def sign_in_new_phase(config, phase_output,
                                                                                                 config[user]['telegram_id'],        # noqa: E501
                                                                                                 'Answers are sent.'))               # noqa: E501
             # send application form
-            python_button3 = browser.find_element_by_xpath(
-                "/html/body/lead-form/bpd-modal/div/div/bpd-pages/div[1]/bpd-form/form/div[1]/div[14]/button")                      # noqa: E501
+            python_button3 = browser.find_element(By.XPATH,
+                                                  "/html/body/lead-form/bpd-modal/div/div/bpd-pages/div[1]/bpd-form/form/div[1]/div[14]/button")                      # noqa: E501
             logging.info('Button 3 is clicked - sending the applicationform for the combination {}, {}.'.format(user,               # noqa: E501
                                                                                                                 housenumber))       # noqa: E501
             action.move_to_element(python_button3).click().perform()
@@ -160,21 +161,17 @@ if __name__ == "__main__":
     phase_output = check_phase_status(config)
 
     # Define settings for running the sign-up script
-    usernames = [config['default']['user_1'],
-                 config['default']['user_2'],
-                 config['default']['user_3'],
-                 config['default']['user_4']]
+    usernames = [config['default']['user_1'], config['default']['user_2'], config['default']['user_3'], config['default']['user_4']]                 
     chatbot_id = config['default']['chatbot_id']
     chatbot_key = config['default']['chatbot_key']
     chatbot_id2 = config['default']['chatbot_id2']
     chatbot_key2 = config['default']['chatbot_key2']
-    phase_check = config['default']['fase_check']
+    phase_check = config['default']['fase_check']          
 
     # if new phase is availble sign-up for each user for different housenumbers
     if phase_check in set(phase_output):
         for user in usernames:
-            housenumbers = [config[user]["housenumber1"],
-                            config[user]["housenumber2"]]
+            housenumbers = [config[user]["housenumber1"], config[user]["housenumber2"]]                 # noqa: E501
             sign_in_new_phase(config, phase_output, housenumbers,
                               chatbot_id, chatbot_key)
     else:
